590f9fb8c11fe033cadbe64640dbd32d09e930a7
liaowenqiang <18840965351@163.com>
2023-09-03 17:29:24 +0800

    [wenqwiang] feat: add default slot demo

---
+++ vue_slot_demo/src/components/ChildDefaultSlot.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <span class="container">this is children componet</span>
+    <!-- 默认插槽 -->
+    <slot>this is slot default content in child</slot>
+</template>
+
+<style scoped>
+.container {
+    color: rgb(19, 68, 52);
+}
+</style>
---
+++ vue_slot_demo/src/components/Parent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import ChildDefaultSlot from './ChildDefaultSlot.vue';
+
+
+</script>
+
+<template>
+    <h1 class="container">this is parent component</h1>
+    <ChildDefaultSlot>
+        <h2>this is content pass from parent and want to be rendered in child position</h2>
+    </ChildDefaultSlot>
+</template>
+
+<style scoped>
+.container {
+    color: brown;
+}
+</style>
